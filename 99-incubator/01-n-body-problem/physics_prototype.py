29d8781a372c9ce6dd29582dcf4331c2291833df
Daniel Ope <opeoluwaoyedeji06@gmail.com>
2025-12-16 22:18:06 +0000

    chore: Test running simulation for 200yrs

--- 99-incubator/01-n-body-problem/physics_prototype.py
+++ 99-incubator/01-n-body-problem/physics_prototype.py
@@ -5,15 +5,41 @@
 # **Time:** days
 
 # %%
-from math_utils import (get_initial_conditions, plot_initial_conditions)
+from math_utils import (
+    get_initial_conditions,
+    plot_initial_conditions,
+    plot_trajectory
+)
 
-INITIAL_CONDITION = "solar_system"
-system, labels, colors, legend = get_initial_conditions(INITIAL_CONDITION)
+# %%
+INITIAL_CONDITION: str = "solar_system"
+n_body_system, labels, colors, legend = get_initial_conditions(
+    INITIAL_CONDITION)
 plot_initial_conditions(
-    system=system,
+    system=n_body_system,
     labels=labels,
     colors=colors,
     legend=legend,
+)
+
+# %%
+TIME_FRAME: float = 200.0 * 365.24  # 200 years to days
+TIME_STEP: float = 1.0
+OUTPUT_INTERVAL: float = 0.1 * 365.24
+
+# Launch Simulation
+pos_history, vel_history, time_history = n_body_system.run(
+    time_frame=TIME_FRAME,
+    time_step=TIME_STEP,
+    output_interval=OUTPUT_INTERVAL
+)
+
+# %%
+plot_trajectory(
+    sol_x=pos_history,
+    labels=labels,
+    colors=colors,
+    legend=legend
 )
 
 # %%
