7cf152a1aacb0858398eddcbc93f0d1887431bc4
Daniel Ope <opeoluwaoyedeji06@gmail.com>
2026-02-27 13:26:14 +0000

    feat: set up version 2 of hill climb

--- 03-optimisation/one-variable-optimisation/hill_climb.py
+++ 03-optimisation/one-variable-optimisation/hill_climb.py
@@ -9,15 +9,35 @@ def f(x):
     return f
 
 
-xbest = random.uniform(0, 10)
-fbest = f(xbest)
-steps = 100
+# xbest = random.uniform(0, 10)
+# fbest = f(xbest)
+# steps = 100
 
-for i in range(1, steps):
-    xnew = xbest + random.gauss(0, 0.1)  # Hill climb step
-    fnew = f(xnew)
-    if fnew > fbest:
-        xbest = xnew
-        fbest = fnew
+# for i in range(1, steps):
+#     xnew = xbest + random.gauss(0, 0.1)  # Hill climb step
+#     fnew = f(xnew)
+#     if fnew > fbest:
+#         xbest = xnew
+#         fbest = fnew
 
-print('xbest', xbest, 'fbest', fbest)
+# print('xbest', xbest, 'fbest', fbest)
+
+def hill_climb_2():
+    for _ in range(10):
+        xbest = random.uniform(0, 10)
+        fbest = f(xbest)
+        steps = 100
+
+        for i in range(1, steps):
+            xnew = xbest + random.gauss(0, 0.1)  # Hill climb step
+            fnew = f(xnew)
+            if fnew > fbest:
+                xbest = xnew
+                fbest = fnew
+
+        print('xbest', xbest, 'fbest', fbest)
+
+
+hill_climb_2()
+
+# xbest 4.901592491460627 fbest 5448.892101754391
