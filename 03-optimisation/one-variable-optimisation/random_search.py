4a42025b60142bfbfecb42a0ba9228716ab70fc9
Daniel Ope <opeoluwaoyedeji06@gmail.com>
2026-02-27 13:33:41 +0000

    feat: perform random search on second function

--- 03-optimisation/one-variable-optimisation/random_search.py
+++ 03-optimisation/one-variable-optimisation/random_search.py
@@ -1,12 +1,15 @@
 import random
 # import numpy
-# import math
+import math
 
+
+# def f(x):
+#     f = -x**6 + 28*x**5 - 307*x**4 + 1660*x**3 - 4564 * \
+#         x**2 + 5872*x + 2688  # Change this for each question
+#     return f
 
 def f(x):
-    f = -x**6 + 28*x**5 - 307*x**4 + 1660*x**3 - 4564 * \
-        x**2 + 5872*x + 2688  # Change this for each question
-    return f
+    return (math.sin(x) * math.cos(6*x))
 
 
 # xbest = random.uniform(0, 10)
@@ -25,10 +28,10 @@ def f(x):
 
 # Version 2
 def random_search_2():
-    for j in range(10):
+    for _ in range(10):
         xbest = random.uniform(0, 10)
         fbest = f(xbest)
-        steps = 100
+        steps = 500
 
         for i in range(1, steps):
             xnew = random.uniform(0, 10)  # new random solution
@@ -42,4 +45,8 @@ def random_search_2():
 
 random_search_2()
 
+# Q1
 # xbest 1.3903672898291042 fbest 5482.14490302401
+
+# Q2
+# xbest 4.7119142812789985 fbest 0.9999958312272437
